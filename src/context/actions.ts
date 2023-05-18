0946cedb9ed8b4e32f42af922ad22ea072049f1d
Wilder Rincon <wildsrincon.developer@gmail.com>
2023-05-18 14:46:24 -0500

    feat: Implement drag and drop cards columns

--- src/context/actions.ts
+++ src/context/actions.ts
@@ -1,6 +1,7 @@
 export type Action =
 | { type: "ADD_LIST", payload: string }
 | { type: "ADD_TASK", payload: { text: string, listId: string } }
+| { type: "MOVE_LIST", payload: { draggedId: string, hoverId: string } }
 
 export const addTask = (text: string, listId: string): Action => ({
     type: "ADD_TASK",
@@ -13,4 +14,12 @@ export const addTask = (text: string, listId: string): Action => ({
 export const addList = (text: string): Action => ({
   type: "ADD_LIST",
   payload: text
+})
+
+export const moveList = (draggedId: string, hoverId: string): Action => ({
+  type: "MOVE_LIST",
+  payload: {
+    draggedId,
+    hoverId
+  }
 })
--- src/context/appStateReducer.ts
+++ src/context/appStateReducer.ts
@@ -1,6 +1,6 @@
 import { Action } from "./actions";
 import { nanoid } from "nanoid";
-import { findItemIndexById } from "../utils/arrayUtils";
+import { findItemIndexById, moveItem } from "../utils/arrayUtils";
 
 export type Task = {
   id: string;
@@ -36,6 +36,12 @@ export const appStateReducer = (draft: AppState, action: Action): AppState | voi
         text
       })
       break;
+    }
+    case "MOVE_LIST": {
+      const { draggedId, hoverId } = action.payload;
+      const dragIndex = findItemIndexById(draft.lists, draggedId);
+      const hoverIndex = findItemIndexById(draft.lists, hoverId);
+      draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
     }
   }
 };
